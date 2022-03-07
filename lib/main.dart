68e4336eaac754c3f7a2387b892d4156501727dc
jorgehiler <andreshiler@gmail.com>
2022-03-06 22:36:49 -0500

    Remove comments

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -53,27 +51,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _onChange(index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _currentIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: IndexedStack(
@@ -104,7 +89,7 @@ class _MyHomePageState extends State<MyHomePage> {
             label: 'QR',
           )
         ],
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
